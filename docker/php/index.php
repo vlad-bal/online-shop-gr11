70184379563b6807c03ab0145bc9aba986ba723c
vlad-bal <vladbalzhinimaev5gmail.com>
2024-11-13 17:31:17 +0800

    Hello

--- docker/php/index.php
+++ docker/php/index.php
@@ -1,8 +1,4 @@
 <?php
-$conn = pg_connect("host=postgres_db dbname=mydb user=user password=pwd");
-if ($conn) {
-    echo "Connected to PostgreSQL successfully";
-} else {
-    echo "Connection failed.";
-}
+echo "Hello world";
+
 ?>
